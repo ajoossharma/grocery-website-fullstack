bfcbc7dabdb2f7220250935df8d346e659d2b232
ajoossharma <165235263+ajoossharma@users.noreply.github.com>
2025-05-16 05:17:15 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173', '']
+const allowedOrigins = ['http://localhost:5173', 'https://grocery-website-fullstack-2r2t.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
